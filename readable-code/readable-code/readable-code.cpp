2b659172fad0f7fb61aa3627f95b29da55087fd7
yamashiro000 <n-yamashiro@system-square.co.jp>
2015-03-06 11:13:29 +0900

    spec1:オムライス表示

---
+++ readable-code/readable-code/readable-code.cpp
@@ -0,0 +1,14 @@
+// readable-code.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("オムライス");
+	//_getch();
+	//Sleep(1000);
+	return 0;
+}
+
