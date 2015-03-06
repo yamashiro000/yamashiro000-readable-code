9b039534f2a501bb8ee766903e03a3129e672a78
yamashiro000 <n-yamashiro@system-square.co.jp>
2015-03-06 12:24:20 +0900

    Spec4：作業途中

--- readable-code/readable-code/README.cpp
+++ readable-code/readable-code/README.cpp
@@ -1,15 +1,17 @@
 開発言語：C++
 
-Spec2:実行方法
+実行方法
 	Visual Studio2010の開発環境に依存しているため
 	Visual Studio2010をインストールして新規プロジェクトを作成し
 	Githubリポジトリyamashiro000/yamashiro000-readable-codeから
 	readable-code.cppをクローンしてプロジェクトにファイル追加し
 	ビルドして実行する。
 
-Spec3:レシピデータの分離&レシピデータ読込
-	レシピデータをrecipe-data.txtとして分離し
 	recipe-data.txtをCドライブの直下に置く。
 	ビルドしたプログラムを実行すると
 	Cドライブの直下のrecipe-data.txtを読み込んで
 	コンソールにレシピデータを表示する。
+
+	recipe-data.txtに親子丼、杏仁豆腐を追加。
+
+
