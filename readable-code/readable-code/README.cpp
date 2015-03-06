d5e32e7f4e0824b89428173ae91716f3898a5e76
yamashiro000 <n-yamashiro@system-square.co.jp>
2015-03-06 11:38:46 +0900

    Spec2:実行方法

--- readable-code/readable-code/README.cpp
+++ readable-code/readable-code/README.cpp
@@ -1,1 +1,8 @@
 開発言語：C++
+
+Spec2:実行方法
+	Visual Studio2010の開発環境に依存しているため
+	Visual Studio2010をインストールして新規プロジェクトを作成し
+	Githubリポジトリyamashiro000/yamashiro000-readable-codeから
+	readable-code.cppをクローンしてプロジェクトにファイル追加し
+	ビルドして実行する。
