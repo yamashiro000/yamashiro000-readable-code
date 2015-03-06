bf45693b4dfcaee3dc30e26d67f0efb68432975a
yamashiro000 <n-yamashiro@system-square.co.jp>
2015-03-06 12:08:34 +0900

    Step3:レシピデータの分離&レシピデータ読込

--- readable-code/readable-code/readable-code.cpp
+++ readable-code/readable-code/readable-code.cpp
@@ -2,13 +2,32 @@
 //
 
 #include "stdafx.h"
+#include "windows.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("オムライス");
-	//_getch();
-	//Sleep(1000);
+	FILE *fp;			// ファイルポインタの宣言
+	char strRecipe[256];// レシピデータ用256バイトの配列
+	
+	/* ファイルのオープン */
+	if ((fp = fopen("C:\\recipe-data.txt", "r")) == NULL) 
+	{// エラーの場合は通常、異常終了する
+		printf("file open error!!\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	while (fgets(strRecipe, 256, fp) != NULL) 
+	{// ここではfgets()により１行単位で読み出し
+		printf("%s", strRecipe);
+	}
+
+	/* ファイルのクローズ */
+	fclose(fp);
+	
+	/* コンソール表示残すためのSleep() */
+	Sleep(1000);
+
 	return 0;
 }
 
